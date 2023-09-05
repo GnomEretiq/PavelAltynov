a2fc3423c1028be16c8b0b865bb9729f14a1e6b4
Pavel <user080902@gmail.com>
2023-09-05 17:08:26 +0300

    t1

--- PavelAltynov/CoffeMachine/src/Main.java
+++ PavelAltynov/CoffeMachine/src/Main.java
@@ -1,3 +1,8 @@
 public class Main {
-    public static void main(String [])
+    public static void main(String[] args) {
+        int coffeAmount = 2330;
+        int milkAmount = 3210;
+
+        int cappuchino
+    }
 }

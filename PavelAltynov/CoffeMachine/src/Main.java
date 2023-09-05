5c415bba46555a00467fc6a7c3b3b74751f3133d
Pavel <user080902@gmail.com>
2023-09-05 17:26:35 +0300

    t1

--- PavelAltynov/CoffeMachine/src/Main.java
+++ PavelAltynov/CoffeMachine/src/Main.java
@@ -2,7 +2,26 @@ public class Main {
     public static void main(String[] args) {
         int coffeAmount = 2330;
         int milkAmount = 3210;
+        int skimmedMilkAmount = 1290;
+        boolean isBloked = true;
 
-        int cappuchino
+        int cappuchinoMilkRequired = 60;
+        int cappucianoCoffeRequired = 15;
+
+        if(!isBloked){
+            System.out.println("Попробуем приготовить кофе");
+        }else {
+            System.out.println("Кофемашина заблокирована");
+        }
+        if(skimmedMilkAmount >= cappuchinoMilkRequired || milkAmount >= cappuchinoMilkRequired){
+            System.out.println("Молока достаточно");
+        }else {
+            System.out.println("Молока недостаточно");
+        }
+        if(coffeAmount >= cappuchinoMilkRequired && milkAmount >= cappuchinoMilkRequired) {
+            System.out.println("Готовим капучино");
+        } else {
+            System.out.println("Ингридиентов недостаточно =(");
+        }
     }
 }
